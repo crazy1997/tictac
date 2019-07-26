c437d19b27992f8f9bf96875b3ef0a441db547c4
crazy1997 <silap1997@gmail.com>
2019-07-27 00:39:06 +0300

    production with

---
+++ tictac/build/precache-manifest.8e7b995f663333010d83ede04478a774.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "046a86f91014015c069bf88289bb0ad6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28db4e5374dc0c5d9d12",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "28db4e5374dc0c5d9d12",
+    "url": "/static/js/main.b441efcd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
